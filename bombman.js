ef6b78a36a146770dc0e11a69cfb1779b7d74e61
arjusahu <arjusahu12@gmail.com>
2021-01-24 11:33:24 +0530

    BomberMan

--- bombman.js
+++ bombman.js
@@ -4,17 +4,16 @@ let blocks = document.getElementsByClassName("blocks")[0];
 let points = document.getElementsByClassName("points")[0];
 let score = document.getElementById("score");
 let newdiv = document.createElement("div");
-let count = 0;
+let count = 0, score1 = "";
 let arr = [];
 button.addEventListener("click", formationOfBlocks);
 
 function formationOfBlocks() {
-
     newdiv.innerHTML = "";
     score.innerHTML = "0";
     points.style.display = "inline-block";
     about.style.display = "none";
-    //cretes random boxes where box are placed
+    //cretes random boxes where bomb are placed
     for (let ran = 0; ran < 10; ran++) {
         let no = Math.ceil(Math.random() * 81);
         if (!arr.includes(no)) {
@@ -36,60 +35,48 @@ function formationOfBlocks() {
 
 
 
-
-
-let copy1 = 0;
+//After clicking on boxes
 function editbox(i) {
     button.innerHTML = "Restart";
-    let score1 = document.getElementById("score").innerHTML;
-    if (score1 == 70)//he will win after 70 click
-    {
-        let msg = document.getElementsByClassName("msg")[0]
-        newdiv.setAttribute("id", "lost");
-        newdiv.setAttribute("style", "font-size:11em;background-Color: rgba(133, 126, 187, 0.8);font-weight:bold;text-shadow:1px 1px 5px black;color:green");
-
-        newdiv.innerHTML = "YOU WON 😃 ";
-        msg.appendChild(newdiv);
-        for (let even = 1; even <= 81; even++) {
-            let v1 = document.getElementById(even);
-            v1.onclick = "final()";
-        }
-
-    }
-    let copy2 = copy1;
-    copy1 = i;
-    if (copy1 != copy2) {
-        if (copy2 != 0) {
-            let boxafter = document.getElementById(copy2);
-            boxafter.style.backgroundColor = "white";
-        }
-        if (score1 == "")
-            count = 1;
-        else {
-            count = parseInt(score1) + 1;
-        }
-
-
-        if (arr.includes(parseInt(i))) {
-            showBomb(i);
-        }
-
-        else {
-            score.innerHTML = count;
-            let box = document.getElementById(i)
-            box.style.backgroundColor = "green";
-
+    score1 = document.getElementById("score").innerHTML;
+    let box = document.getElementById(i)
+    if (score1 == "")
+        count = 1;
+    else if (box.style.backgroundColor == "white") {
+        count = parseInt(score1) + 1;
+        if (count == 71) {
+            won();
         }
     }
+    if (arr.includes(parseInt(i))) {
+        showBomb(i);
+    }
+
+    else {
+        score.innerHTML = count;
+        box.style.backgroundColor = "green";
+
+    }
+
 }
+function won() {
+    let msg = document.getElementsByClassName("msg")[0]
+    newdiv.setAttribute("id", "lost");
+    newdiv.setAttribute("style", "font-size:11em;background-Color: rgba(133, 126, 187, 0.8);font-weight:bold;text-shadow:1px 1px 5px black;color:green");
 
+    newdiv.innerHTML = "YOU WON 😃 ";
+    msg.appendChild(newdiv);
+    for (let even = 1; even <= 81; even++) {
+        let v1 = document.getElementById(even);
+        v1.onclick = "final()";
+
+    }
+
+}
 
 
 // function to show bomb 
 function showBomb(i) {
-    let bomb = document.getElementById(i);
-    bomb.style.backgroundColor = "red";
-    bomb.style.backgroundImage = "url('https://img.icons8.com/emoji/48/000000/bomb-emoji.png')";
     for (let rand = 0; rand < 10; rand++) {
         let bomb = document.getElementById(arr[rand]);
         bomb.style.backgroundColor = "red";
@@ -97,11 +84,13 @@ function showBomb(i) {
         bomb.style.backgroundSize = "cover";
 
     }
+    // to remove events
     for (let even = 1; even <= 81; even++) {
         let v1 = document.getElementById(even);
         v1.onclick = "final()";
 
     }
+    // to show lost message
     let msg = document.getElementsByClassName("msg")[0]
     newdiv.setAttribute("id", "lost");
     newdiv.setAttribute("style", "font-size:11em;background-Color: rgba(133, 126, 187, 0.8);font-weight:bold;text-shadow:1px 1px 5px black;color:red");
